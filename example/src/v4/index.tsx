7e867ad118a637e101d07937f6a8a5885873f261
Dipesh Rai <raidipesh78@gmail.com>
2024-07-24 11:38:43 +0545

    fix: timing track animation done

--- example/src/v4/index.tsx
+++ example/src/v4/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { v4 } from '@raidipesh78/re-motion';
 import { useDrag } from '@use-gesture/react';
 
@@ -7,8 +7,7 @@ const { FluidValue, makeFluid, spring } = v4;
 const FluidDiv = makeFluid('div');
 
 const App = () => {
-  const [a, setA] = useState(0);
-  const x = useRef(new FluidValue(100)).current;
+  const x = useRef(new FluidValue(0)).current;
   const x1 = useRef(new FluidValue(0)).current;
 
   const bind = useDrag(({ offset: [ox] }) => {
@@ -22,7 +21,6 @@ const App = () => {
 
       <button
         onClick={() => {
-          setA((p) => p + 1);
           spring(x, { toValue: 300 });
           spring(x1, { toValue: x });
         }}
@@ -41,7 +39,6 @@ const App = () => {
         }}
       />
       <FluidDiv
-        data-a={a}
         style={{
           width: 100,
           height: 100,
--- packages/re-motion/src/v4/controllers/timing.ts
+++ packages/re-motion/src/v4/controllers/timing.ts
@@ -1,11 +1,26 @@
 import { EndResultType } from '../animations/FluidAnimation';
 import { Timing, TimingConfig } from '../animations/Timing';
+import { Fluid } from '../fluids/Fluid';
+import { FluidTrack } from '../fluids/FluidTrack';
 import { FluidValue } from '../fluids/FluidValue';
 
 export const timing = (
   value: FluidValue,
-  config: TimingConfig,
+  config: Omit<TimingConfig, 'toValue'> & { toValue: number | FluidValue },
   callback?: (value: EndResultType) => void
 ) => {
-  value.animate(new Timing(config), callback);
+  value.stopTrack();
+  if (config.toValue instanceof Fluid) {
+    value.startTrack(
+      new FluidTrack(value, config.toValue, Timing, config, callback)
+    );
+  } else {
+    value.animate(
+      new Timing({
+        ...config,
+        toValue: config.toValue,
+      }),
+      callback
+    );
+  }
 };
