9feb1e710cbc8bed57854025d7558acb3c6aa229
Dipesh Rai <raidipesh78@gmail.com>
2024-07-27 17:30:05 +0545

    fix: removed the react hooks

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,11 @@
 import { useLayoutEffect } from 'react';
-// import SharedElement from './examples/SharedElement';
-
-// import Example from './components/Decay';
-// import Example from './components/Interpolation';
-// import Example from './components/SequenceTransition';
-import Example from './components/UseFluidValue';
-// import Example from './components/UseMount';
 
 const App = () => {
   useLayoutEffect(() => {
     document.body.style.backgroundColor = '#333';
   }, []);
 
-  return (
-    <>
-      <Example />
-    </>
-  );
+  return <>APP COMPONENT</>;
 };
 
 export default App;
--- packages/re-motion/src/index.ts
+++ packages/re-motion/src/index.ts
@@ -2,16 +2,6 @@ export { FluidValue } from './fluids/FluidValue';
 export { makeFluid } from './fluids/makeFluid';
 export { fluid } from './fluids/tags';
 export { Easing } from './easing/Easing';
-export { interpolate } from './controllers/interpolate';
 
 // animation controllers
 export { timing, spring, decay } from './controllers';
-
-// hooks
-export { useFluidValue } from './hooks/useFluidValue';
-export { useMount } from './hooks/useMount';
-
-// types
-export type { UseFluidValueConfig } from './hooks/useFluidValue';
-export type { UseMountConfig } from './hooks/useMount';
-export type { ExtrapolateConfig } from './controllers/interpolate';
