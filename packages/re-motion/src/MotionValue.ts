89dafe31baf823f6e7a41a1d85aa9f609905f9b0
Dipesh Rai <raidipesh78@gmail.com>
2025-05-15 06:47:47 +0545

    fix: refactor

--- packages/re-motion/src/MotionValue.ts
+++ packages/re-motion/src/MotionValue.ts
@@ -3,80 +3,6 @@ import { isCssColorLiteral, parseCssColor } from './colorsUtils';
 
 type Subscriber<T> = (v: T) => void;
 
-export class MotionValue<T = number> {
-  private subs = new Set<Subscriber<T>>();
-  private _current: T;
-  private initial: T;
-  private currentController?: AnimationController;
-
-  constructor(initial: T) {
-    this._current = initial;
-    this.initial = initial;
-  }
-
-  get current(): T {
-    return this._current;
-  }
-
-  set(v: T): void {
-    this.currentController?.cancel();
-    this.currentController = undefined;
-    this._internalSet(v);
-  }
-
-  /**
-   * @internal only use internally
-   */
-  _internalSet(v: T): void {
-    if (v === this._current) return;
-    this._current = v;
-    for (const sub of this.subs) sub(v);
-  }
-
-  subscribe(fn: Subscriber<T>): () => void {
-    this.subs.add(fn);
-    fn(this._current);
-    return () => {
-      this.subs.delete(fn);
-    };
-  }
-
-  reset() {
-    this.set(this.initial);
-  }
-
-  destroy() {
-    this.subs.clear();
-    this.currentController?.cancel();
-    this.currentController = undefined;
-  }
-
-  to<U>(mapperFn: (v: T) => U): MotionValue<U>;
-  to(
-    inRange: number[],
-    outRange: (number | string)[],
-    easing?: (t: number) => number
-  ): MotionValue<number | string>;
-  to(arg1: any, arg2?: any, arg3?: any): MotionValue<any> {
-    if (typeof arg1 === 'function') {
-      const mapFn = arg1 as (v: T) => any;
-      const out = new MotionValue(mapFn(this._current));
-      this.subscribe((v) => out.set(mapFn(v)));
-      return out;
-    }
-
-    const inRange = arg1 as number[];
-    const outRange = arg2 as (number | string)[];
-    const easing = (arg3 as (t: number) => number) || ((t: number) => t);
-    return interpolate(this as MotionValue<number>, inRange, outRange, easing);
-  }
-
-  setAnimationController(ctrl: AnimationController) {
-    this.currentController?.cancel();
-    this.currentController = ctrl;
-  }
-}
-
 function interpolate(
   input: MotionValue<number>,
   inRange: number[],
@@ -207,4 +133,78 @@ export function combine<T extends any[], U>(
   inputs.map((fv) => fv.subscribe(() => update()));
 
   return out;
+}
+
+export class MotionValue<T = number> {
+  private subs = new Set<Subscriber<T>>();
+  private _current: T;
+  private initial: T;
+  private currentController?: AnimationController;
+
+  constructor(initial: T) {
+    this._current = initial;
+    this.initial = initial;
+  }
+
+  get current(): T {
+    return this._current;
+  }
+
+  set(v: T): void {
+    this.currentController?.cancel();
+    this.currentController = undefined;
+    this._internalSet(v);
+  }
+
+  /**
+   * @internal only use internally
+   */
+  _internalSet(v: T): void {
+    if (v === this._current) return;
+    this._current = v;
+    for (const sub of this.subs) sub(v);
+  }
+
+  subscribe(fn: Subscriber<T>): () => void {
+    this.subs.add(fn);
+    fn(this._current);
+    return () => {
+      this.subs.delete(fn);
+    };
+  }
+
+  reset() {
+    this.set(this.initial);
+  }
+
+  destroy() {
+    this.subs.clear();
+    this.currentController?.cancel();
+    this.currentController = undefined;
+  }
+
+  to<U>(mapperFn: (v: T) => U): MotionValue<U>;
+  to(
+    inRange: number[],
+    outRange: (number | string)[],
+    easing?: (t: number) => number
+  ): MotionValue<number | string>;
+  to(arg1: any, arg2?: any, arg3?: any): MotionValue<any> {
+    if (typeof arg1 === 'function') {
+      const mapFn = arg1 as (v: T) => any;
+      const out = new MotionValue(mapFn(this._current));
+      this.subscribe((v) => out.set(mapFn(v)));
+      return out;
+    }
+
+    const inRange = arg1 as number[];
+    const outRange = arg2 as (number | string)[];
+    const easing = (arg3 as (t: number) => number) || ((t: number) => t);
+    return interpolate(this as MotionValue<number>, inRange, outRange, easing);
+  }
+
+  setAnimationController(ctrl: AnimationController) {
+    this.currentController?.cancel();
+    this.currentController = ctrl;
+  }
 }
