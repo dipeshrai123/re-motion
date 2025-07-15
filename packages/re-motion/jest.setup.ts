1b3081a8bd849587f5ef217e2ca027aeeabb33d7
Dipesh Rai <raidipesh78@gmail.com>
2025-07-15 20:26:11 +0545

    test: motion component test complete

--- packages/re-motion/jest.setup.ts
+++ packages/re-motion/jest.setup.ts
@@ -1,3 +1,7 @@
+const TestUtils = require('react-dom/test-utils');
+const React = require('react');
+TestUtils.act = React.act;
+
 global.requestAnimationFrame = (cb: FrameRequestCallback): number => {
   return setTimeout(() => cb(performance.now()), 16) as unknown as number;
 };
